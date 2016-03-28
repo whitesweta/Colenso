132dc90aedf374d90a142d13ae95ee8512a4e75a
Shweta Barapatre <sb1520@l3129.local>
2016-03-28 14:24:56 +1300

    Scrolling nav bar functioning

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var basex = require('basex');
+var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
+
+client.execute("OPEN Colenso");
+
+client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; " +
+    "//name[@type = 'place' and position() = 1 and . = 'Manawarakau']",
+    function(err,res) { if(!err) console.log(res.result)} );
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
