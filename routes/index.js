c129e18f269ba9dcdadfc95de0c36fec11f03a52
Shweta Barapatre <sb1520@l3129.local>
2016-03-30 07:14:41 +1300

    added see all files functionality and added view raw

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,15 @@ var router = express.Router();
 var xml_lines = null;
 var firstLocation = [];
 var uniquePlaces = [];
+var Colenso = [];
+var Haast = [];
+var Hadfield = [];
+var Hector = [];
+var Holmes = [];
+var Hooker = [];
+var Mclean = [];
+var Other = [];
+var fileName = "";
 
 
 //FOR BROWSE - INCOMPLETE//
@@ -22,14 +31,35 @@ client.execute("XQUERY db:list('Colenso')",
 				for (var i = 0; i < xml_lines.length; ++i) {
   					var res = xml_lines[i].split("/");
   					firstLocation.push(res[0]);
+  					if(res[0]==="Haast"){
+  						Haast.push(xml_lines[i]);
+  					}
+  					else if(res[0]==="Hector"){
+  						Haast.push(xml_lines[i]);
+  					}
+  					else if(res[0]==="Holmes"){
+  						Holmes.push(xml_lines[i]);
+  					}
+  					else if(res[0]==="Hooker"){
+  						Hooker.push(xml_lines[i]);
+  					}
+  					else if(res[0]==="Mclean"){
+  						Mclean.push(xml_lines[i]);
+  					}
 				}
+				//use this to populate nav bar?? i don't know how to do this so its hard coded
 				uniquePlaces = unique(firstLocation);
-				for (var i = 0; i < uniquePlaces.length; ++i) {
-  					console.log('value at index [' + i + '] is: [' + uniquePlaces[i] + ']');
-  			}
 			}
 		}
 	)
+
+//POPULATE BROWSE - WORKING ON IT
+/*router.get("/browseAuther",function(req,res){
+      var TEIs = result.result.split('\n');
+      var length = TEIs.length;
+      res.render('search', {files: TEIs, searchString: query, numResults: length});
+
+});*/
 
 
 //VIEW FILE IN NICE READABLE TEXT
@@ -47,11 +77,60 @@ function (error, result) {
 	});
 });
 
+//VIEW RAW
+router.get("/viewRaw",function(req,res){
+client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+  	"(doc('Colenso/"+fileName+"'))[1]",
+function (error, result) {
+  if(error){
+	   console.error(error);
+	  }
+	else {
+    res.render('viewRaw', {file: result.result });
+	 }
+	});
+});
+
+//DOWNLOAD. DONT KNOW WHAT THIS
+router.get("/download",function(req,res,next){
+	
+	var url = req.originalUrl;
+	var path = url.replace('/download/', '');
+	client.execute("XQUERY doc('"+path+"')",
+		function(error, result) {
+			if (error) {
+				console.error(error);
+			}
+			else {
+				var doc = result.result;
+				var filename = 'tei_document.xml';
+				res.writeHead(200, {
+					'Content-Disposition': 'attachment; filename=' + filename,
+				});
+				res.write(doc);
+				res.end();
+			}
+		}
+	)
+});
+
+router.get("/All",function(req,res){
+
+      var TEIs = xml_lines;
+      var length = TEIs.length;
+      res.render('All', {files: TEIs, numResults: length});
+
+});
 
 //SEARCH BY STRING
 router.get("/search",function(req,res){
   var query = req.query.searchString;
-  client.execute(("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" + "for $v in .//TEI[. contains text "+query+"] return db:path($v)"),
+  var fullQuery = query
+  			.replace(" AND ", '\' ftand \'')
+			.replace(" NOT ", '\' ftnot \'')
+			.replace(" OR ", '\' ftor \'');
+
+  client.execute(("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" + "for $v in .//TEI[. contains text "+fullQuery+"] return db:path($v)"),
   function (error, result) {
     if(error){
 	     console.error(error);
@@ -76,7 +155,8 @@ router.get("/searchxquery", function(req,res){
     else{
       var TEIs = result.result.split('\n');
       var length = TEIs.length;
-      res.render('searchxquery',{files: TEIs, searchString: query, numResults: length});
+      var raw = false;
+      res.render('searchxquery',{files: TEIs, searchString: query, numResults: length, isRaw: raw});
     }
   });
 });
